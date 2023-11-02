fbb21c73c91fa22eef76a58dbb85999137c6b3c5
farhanmahbubi <farhanmahbuby99@gmail.com>
2023-11-03 01:26:56 +0700

    penjelasan

--- app/src/main/java/com/example/junit/Calculator.kt
+++ app/src/main/java/com/example/junit/Calculator.kt
@@ -2,7 +2,7 @@ package com.example.junit
 
 //calculator test
 class Calculator {
-    fun add(first: Int, second: Int): Int {
-        return second + first
+    fun add(first: Int, second: Int): Int {     // Metode add menerima dua argumen bertipe data Int, first dan second.
+        return first + second                   // Metode ini mengembalikan hasil penjumlahan dari first + second.
     }
 }
