2582401c78da44fa29764a105b4e4cca72ec4adc
farhanmahbubi <farhanmahbuby99@gmail.com>
2023-11-03 01:12:53 +0700

    calculator test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.junit"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.junit"
---
+++ app/src/main/java/com/example/junit/Calculator.kt
@@ -0,0 +1,8 @@
+package com.example.junit
+
+//calculator test
+class Calculator {
+    fun add(first: Int, second: Int): Int {
+        return second + first
+    }
+}
