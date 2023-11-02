bd3b9cde9f29023d6ca53147ef676f744a89b5ec
farhanmahbubi <farhanmahbuby99@gmail.com>
2023-11-02 20:05:04 +0700

    penjelasan

--- app/src/test/java/com/example/junit/ExampleUnitTest.kt
+++ app/src/test/java/com/example/junit/ExampleUnitTest.kt
@@ -1,11 +1,14 @@
 package com.example.junit
 
-import org.junit.Test
-import org.junit.Assert.*
+import org.junit.Test // Mengimpor anotasi @Test dari JUnit.
+import org.junit.Assert.* // Mengimpor metode asser JUnit untuk pengujian.
 
 class ExampleUnitTest {
-    @Test
+    @Test // Anotasi @Test digunakan untuk menandai metode pengujian.
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertEquals(4, 2 + 2) // Memeriksa apakah 2 + 2 sama dengan 4.
+
+        // Metode `assertEquals` digunakan untuk membandingkan hasil ekspresi (2 + 2) dengan nilai yang diharapkan (4).
+        // Jika hasil dan nilai yang diharapkan sama, tes dianggap berhasil. Jika tidak, tes dianggap gagal.
     }
 }
